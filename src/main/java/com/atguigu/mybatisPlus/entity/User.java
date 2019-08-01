19ac23d54b8762c86affe3e15042137e60cd37c9
cqy <413565455@qq.com>
2019-08-01 21:38:35 +0800

    补充user类注释

--- src/main/java/com/atguigu/mybatisPlus/entity/User.java
+++ src/main/java/com/atguigu/mybatisPlus/entity/User.java
@@ -7,6 +7,8 @@ import lombok.Data;
 @Data
 public class User {
 
+    // #全局设置主键生成策略
+    //mybatis-plus.global-config.db-config.id-type=auto
     @TableId(type = IdType.AUTO) // 主键自增长
     private Long id;
     private String name;
