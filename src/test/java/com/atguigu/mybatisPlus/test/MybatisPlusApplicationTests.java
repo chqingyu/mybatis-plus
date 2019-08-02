ad4812b7083f6201498bf6bfb2760cc7d2e50260
chqingyu <413565455@qq.com>
2019-08-02 08:42:32 +0800

    补充user类注释...

--- src/test/java/com/atguigu/mybatisPlus/test/MybatisPlusApplicationTests.java
+++ src/test/java/com/atguigu/mybatisPlus/test/MybatisPlusApplicationTests.java
@@ -2,13 +2,18 @@ package com.atguigu.mybatisPlus.test;
 
 import com.atguigu.mybatisPlus.entity.User;
 import com.atguigu.mybatisPlus.mapper.UserMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -32,7 +37,7 @@ public class MybatisPlusApplicationTests {
 
         User user = new User();
         user.setName("Helen");
-        user.setAge(18);
+        user.setAge(19);
         user.setEmail("55317332@qq.com");
         int result = userMapper.insert(user);
         System.out.println(result);
@@ -50,6 +55,47 @@ public class MybatisPlusApplicationTests {
         System.out.println(result);
 
     }
+
+
+    @Test
+    public void testSelectById(){
+
+        // 根据id查询记录
+        User user = userMapper.selectById(1L);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testSelectBatchIds(){
+
+        // 通过多个id批量查询
+        List<User> users = userMapper.selectBatchIds(Arrays.asList(1, 2, 3));
+        users.forEach(System.out::println);
+    }
+
+
+    @Test
+    public void testSelectByMap(){
+
+        // 通过map封装查询条件 where name=?and age =?
+        Map<String,Object> map = new HashMap<>();
+        map.put("name","Helen");
+        map.put("age",18);
+        List<User> users = userMapper.selectByMap(map);
+        users.forEach(System.out::println);
+
+    }
+
+    @Test
+    public void testSelectPage(){
+
+        // 测试selectPage分页
+        Page<User> page = new Page<>(1, 5);
+        userMapper.selectPage(page, null);
+        page.getRecords().forEach(System.out::println);
+
+    }
+
 
 
 }
